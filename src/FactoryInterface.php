8fa50358d8bc33f7935ea27b1cc248a4838d9410
vgalitsky <concept.galitsky@gmail.com>
2024-11-29 20:26:43 -0500

    dev

--- src/FactoryInterface.php
+++ src/FactoryInterface.php
@@ -1,46 +1,24 @@
 <?php
 namespace Concept\Factory;
 
-use Concept\Config\ConfigInterface;
 use Psr\Container\ContainerInterface;
 
 /**
  * Interface FactoryInterface
  * @package Concept\Factory
  */
-interface FactoryInterface
+interface FactoryInterface //extends ConfigurableInterface
 {
     /**
      * Create an object
      */
     //public function create(?string $serviceId = null, ...$parameters): object;
-    public function create(): object;
-
-    /**
-     * Set the configuration
-     * 
-     * @param mixed $config
-     */
-    public function withConfig(ConfigInterface $config): self;
-
-    /**
-     * Set the service id
-     * 
-     * @param string $serviceId
-     */
-    //public function withServiceId(string $serviceId): self;
-
-    /**
-     * Set the parameters
-     * 
-     * @param mixed ...$parameters
-     */
-    //public function withParameters(...$parameters): self;
+    public function create();//: object;
 
     /**
      * Set the container
      * 
      * @param ContainerInterface $container
      */
-    //public function withContainer(ContainerInterface $container): self;
+    public function withContainer(ContainerInterface $container): self;
 }
